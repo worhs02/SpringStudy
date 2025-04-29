1580e8f6d42ad9076a26669765643ac76fb75908
Azin <setiguy@gachon.ac.kr>
2025-04-30 03:26:33 +0900

    워크북 최종

---
+++ study5/src/main/java/com/example/study5/domain/User.java
@@ -0,0 +1,56 @@
+package com.example.study5.domain;
+
+import com.example.study5.domain.common.BaseEntity;
+import com.example.study5.domain.enums.Gender;
+import com.example.study5.domain.mapping.Preference;
+import com.example.study5.domain.mapping.UserMission;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+
+public class User extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String name;
+
+    @Column(nullable = false, length = 40)
+    private String address;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(columnDefinition = "VARCHAR(10)")
+    private Gender gender;
+
+
+    private LocalDate inactiveDate;
+
+    private LocalDate updatedDate;
+
+    private LocalDate birth;
+
+    @Column(nullable = false, length = 50)
+    private String email;
+
+    private Integer myPoint;
+
+    private String phone;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<UserMission> userMissions = new ArrayList<>();
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL)
+    private List<Preference> preferences = new ArrayList<>();
+
+}
